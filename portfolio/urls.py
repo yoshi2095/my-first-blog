8ed8afc108508882ffe1b281429859f7e883430f
yoshi2095 <arora4.yoshi@gmail.com>
2016-11-26 20:27:27 +0530

    added views, templates for detailed info  and css for the site.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.project_list, name='project_list'),
+	url(r'^project/(?P<pk>\d+)$', views.project_detail, name='project_detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Project
 
@@ -7,3 +7,8 @@ from .models import Project
 def project_list(request):
 	projects = Project.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'portfolio/project_list.html', {'projects':projects})
+
+def project_detail(request, pk):
+	project = get_object_or_404(Project, pk=pk)
+	return render(request, 'portfolio/project_detail.html', {'project':project})
+
