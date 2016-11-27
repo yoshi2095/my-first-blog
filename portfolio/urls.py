ec468e6b06007a047c6f21d09eefba5799d7e3a1
yoshi2095 <arora4.yoshi@gmail.com>
2016-11-27 16:28:49 +0530

    added views to create/edit projects inside the site.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.project_list, name='project_list'),
 	url(r'^project/(?P<pk>\d+)$', views.project_detail, name='project_detail'),
+	url(r'^project/new/$', views.project_new, name='project_new'),
+	url(r'^project/(?P<pk>\d+)/edit/$', views.project_edit, name='project_edit'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 
@@ -12,3 +13,29 @@ def project_detail(request, pk):
 	project = get_object_or_404(Project, pk=pk)
 	return render(request, 'portfolio/project_detail.html', {'project':project})
 
+def project_new(request):
+	if request.method == "POST":
+		form=ProjectForm(request.POST)
+		if form.is_valid():
+			project=form.save(commit=False)
+			project.author = request.user
+			project.published_date = timezone.now()
+			project.save()
+			return redirect('project_detail', pk=project.pk)
+	else:
+		form = ProjectForm()
+	return render(request, 'portfolio/project_edit.html', {'form':form})
+
+def project_edit(request, pk):
+	project = get_object_or_404(Project, pk=pk)
+	if request.method == "POST":
+		form=ProjectForm(request.POST,instance=project)
+		if form.is_valid():
+			project=form.save(commit=False)
+			project.author = request.user
+			project.published_date = timezone.now()
+			project.save()
+			return redirect('project_detail', pk=project.pk)
+	else:
+		form=ProjectForm(instance=project)
+	return render(request, 'portfolio/project_edit.html', {'form':form})
